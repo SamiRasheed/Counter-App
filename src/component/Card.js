b449e93f762c9fcc81caae23876cd2b30ba7f991
Sami Rasheed <samideveloper7@gmail.com>
2024-09-09 16:27:47 +0500

    Commit
    Counter App

---
+++ src/component/Card.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+export default function Card(props) {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const reset = () => {
+    setCount(0);
+  };
+  const decrement = () => {
+    if (count <= 0) {
+      setCount(0);
+    } else {
+      setCount(count - 1);
+    }
+  };
+  return (
+    <>
+      <div className="header">
+        <h1>{props.heading}</h1>
+      </div>
+      <div className="card my-3">
+        <div className="card-body">
+          <div className="container">
+            <p>{count}</p>
+            <button className="btn btn-danger" onClick={decrement}>
+              -
+            </button>
+            <button className="btn btn-danger" onClick={reset}>
+              Reset
+            </button>
+            <button className="btn btn-danger" onClick={increment}>
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
